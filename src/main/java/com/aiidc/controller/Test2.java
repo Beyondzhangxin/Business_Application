97c6ae186ddbab6c52cab2bbcf1ad03e65bf9951
Jason <573329319@qq.com>
2017-05-11 21:33:56 +0800

    sdafaa

--- src/main/java/com/aiidc/controller/Test2.java
+++ src/main/java/com/aiidc/controller/Test2.java
@@ -4,5 +4,8 @@ package com.aiidc.controller;
  * Created by beyond on 2017/5/11.
  */
 public class Test2 {
-    private String myjob;
+    private String dadad;
+    public void test(){
+        System.out.println("this is the thest!");
+    }
 }
